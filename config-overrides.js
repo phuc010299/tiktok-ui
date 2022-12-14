b343c02a5b3ca3964488c371f84ff90cd3cde620
mac <mac@192.168.1.6>
2022-12-15 01:30:39 +0700

    install babel-plugin-module-resolver

---
+++ config-overrides.js
@@ -0,0 +1,7 @@
+/* config-overrides.js */
+
+const { override, useBabelRc } = require("customize-cra");
+const path = require("path");
+module.exports = override(
+    useBabelRc()
+);
---
+++ src/myAlert.js
@@ -0,0 +1,6 @@
+
+function myAlert() {
+    alert('Hello world')
+}
+
+export default myAlert
